381e0217ecfeabec7badc7e03cba093510640cc4
Tony Chow <tchow9@gmail.com>
2023-02-23 19:30:00 -0800

    word search code and test code

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,27 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['P', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['P', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['L', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APPLE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false the letter array is empty", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if(letters.length === 0) {//Checks if empty array;
+    return false
+  }
+  let verticalArray = transpose(letters);//transposed the array
+  const verticalJoin = verticalArray.map(ls => ls.join(''));//Joins all the elements together into a new array
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let result = false; 
+  for (let l of horizontalJoin) {//Checks horizontally if the word is present
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  for (let v of verticalJoin) {//checks vertically if the word is present
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let finalArr = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let newArr = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newArr.push([matrix[j][i]]);
+    }
+    finalArr.push(newArr);
+  }
+  return finalArr;
+};
+  
+
+module.exports = wordSearch;
